df29d22a6a43b4c14b22be987a0dc9f045707612
Septian Adi Pratama <septianadip7998@gmail.com>
2025-10-30 23:25:57 +0700

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello, World! This is a Node.js server.");
+for (let i = 1; i <= 3; i++) {
+  star = "";
+  for (let j = 1; j <= i; j++) {
+    star += "*";
+  }
+  console.log(star);
+}
